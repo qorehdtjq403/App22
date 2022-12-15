b9920d5c992f898ad0d708eaa543a2db88646aff
qorehdtjq403 <108311468+qorehdtjq403@users.noreply.github.com>
2022-12-15 13:58:14 +0900

    Add files via upload

---
+++ MainWindow.xaml.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "MainWindow.g.h"
+
+namespace winrt::App22::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+    };
+}
+
+namespace winrt::App22::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
